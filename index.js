9fbbc5ebf4ff052fac8f08526dba36b4ce494cdb
Sadat <muhaiminulsadat@gmail.com>
2026-07-03 17:47:23 +0600

    update health check message and add .gitignore file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/health", (req, res) => {
   return res.status(200).json({message: "all is good👍"});
 });
 app.get("/", (req, res) => {
-  return res.status(200).json({message: "hello Sadat v4👍"});
+  return res.status(200).json({message: "hello Sadat v5👍"});
 });
 
 app.listen(port, () => {
